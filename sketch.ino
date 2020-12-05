e6eab844cb2fa941f048c88ec8df4ee8630408f8
Foursevenn <foursevenn.zsq@gmail.com>
2020-12-05 16:39:44 +0000

    filter

--- sketch.ino
+++ sketch.ino
@@ -33,7 +33,7 @@ const byte pHPin         = A1;
 // The PCA9685 is connected to the default I2C connections. There is no need
 // to set these explicitly.
 
-double settemp = 299.00-1.5;
+double settemp = 300.0;
 double heat_error = 0.0;
 
 double setspeed = 1000;
@@ -54,10 +54,18 @@ const double t = 25.0+273.15;
 
 #define FILTER_N 10
 double filter();
-double filter_buf_t[FILTER_N];
-double filter_buf_s[FILTER_N];
-double filter_buf_pH[FILTER_N];
+double filter_buf_t[FILTER_N] = {25.0,25.0,25.0,25.0,25.0,25.0,25.0,25.0,25.0,25.0};
+int tArrayIndex =0;
+double filter_buf_s[FILTER_N] = {1000.0,1000.0,1000.0,1000.0,1000.0,1000.0,1000.0,1000.0,1000.0,1000.0};
+int sArrayIndex =0;
+double filter_buf_pH[FILTER_N] = {5.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0,5.0};
+int pHArrayIndex =0;
 int count = 1;
+
+// #define ArrayLength 10
+// double pHArray[ArrayLength];
+// int pHArrayIndex = 0;
+
 
 void setup() {
   Serial.begin(9600);
@@ -112,11 +120,51 @@ void clear_buf(double buf[]){
   }
 }
 
+double average_array(double* arr, int number){
+  int i;
+  double max,min;
+  double avg;
+  double amount = 0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
+}
+
 void heat(){
   double filtered_t;
   double temp = get_temp();
   filter_buf_t[count] = temp; 
-  heat_error = get_difference(temp,settemp);
+  heat_error = get_difference(temp,settemp-1.5);
   if (heat_error > 5.0){
     analogWrite(heaterPin, 255);
   } else if (heat_error > 3.0){
@@ -130,14 +178,14 @@ void heat(){
   } else{
     analogWrite(heaterPin, 0);
   }
-  if(count == 9){
-  //analogWrite(stirrerPin,100);
-    filtered_t= filter(filter_buf_t);
+  if(count == FILTER_N-1){
+    // tArrayIndex = 0;
+    filtered_t= average_array(filter_buf_t,FILTER_N);
     Serial.print("temperature--> ");
     Serial.print(filtered_t);
     Serial.print("   ");
     Serial.print("temp_error--> ");
-    Serial.print(heat_error);
+    Serial.print(get_difference(filtered_t,settemp));
     Serial.println();
     clear_buf(filter_buf_t); 
   }
@@ -147,18 +195,18 @@ void stir(){
   double filtered_s;
   double speed = get_speed();
   filter_buf_s[count] = speed; 
-  stir_error = get_difference(speed,setspeed);
+  stir_error = get_difference(speed,setspeed+10);
   if (stir_error != 0){
     get_output();
   }
-  if(count == 9){
-  //analogWrite(stirrerPin,100);
-    filtered_s= filter(filter_buf_s);
+  if(count == FILTER_N-1){
+    // sArrayIndex = 0;
+    filtered_s= average_array(filter_buf_s,FILTER_N);
     Serial.print("motor speed--> ");
     Serial.print(filtered_s);
     Serial.print("   ");
     Serial.print("speed_error--> ");
-    Serial.print(stir_error);
+    Serial.print(get_difference(filtered_s,setspeed));
     Serial.println();
     clear_buf(filter_buf_s); 
   }
@@ -188,14 +236,15 @@ void pH(){
      Wire.write(0x0F);
      Wire.endTransmission();
   }
-  if(count == 9){
+  if(count == FILTER_N-1){
   //analogWrite(stirrerPin,100);
-    filtered_pH= filter(filter_buf_pH);
+    sArrayIndex = 0;
+    filtered_pH= average_array(filter_buf_pH,FILTER_N);
     Serial.print("pH--> ");
     Serial.print(filtered_pH);
     Serial.print("   ");
     Serial.print("pH_error--> ");
-    Serial.print(pH_error);
+    Serial.print(get_difference(filtered_pH,setpH));
     Serial.println();
     Serial.println();
     clear_buf(filter_buf_pH); 
